f127045e26765de240ad822a90d8b8ea37fe287b
thiagoalves <thiago.alves@pura.com>
2023-11-28 10:44:48 -0300

    Uploading files

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "KotlinMultiplatformLibrary",
     platforms: [
-        .iOS(.v14.1)
+        .iOS(.v14)
     ],
     products: [
         .library(
